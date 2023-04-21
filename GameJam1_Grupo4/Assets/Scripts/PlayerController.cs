79c1c3deaf0f02d1c7e617782d715f5388f99382
Mauricio Andrés Castro Morales <maocastro13@gmail.com>
2023-04-21 13:18:30 -0500

    PlayerController_MovFull

--- GameJam1_Grupo4/Assets/Scripts/PlayerController.cs
+++ GameJam1_Grupo4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private Animator playerAnim;
 
+    private float horizontalInput;
+    private float speed = 20.0f;
+    private float xRange = 2.30f;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -20,6 +23,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // Check for left and right bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // Player movement left to right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
         if(Input.GetKeyDown(KeyCode.Space) && isOnGround /*&& !gameOver*/)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -36,32 +54,47 @@ public class PlayerController : MonoBehaviour
             hasDoubleJump = true;
         }*/
 
-        /*if(Input.GetKey(KeyCode.LeftShift) && isOnGround)
+        if(Input.GetButton("Sliding") && isOnGround)
         {
-            isSliding = true;
-
-            /*doubleSpeed = true;
-            playerAnim.SetFloat("Speed_Multiplier", 1.5f);
-            dirtParticle.Play();
+            if (!isSliding)
+            {
+                // Iniciar el deslizamiento
+                isSliding = true;
+                playerRb.height /= 2f;
+                playerRb.center = new Vector3(playerRb.center.x, playerRb.center.y / 2f, playerRb.center.z);
+    
+                /*doubleSpeed = true;
+                playerAnim.SetFloat("Speed_Multiplier", 1.5f);
+                dirtParticle.Play();*/
+            }
+            
         }
-        else if (doubleSpeed && speedPowerUp)
+        else
         {
-            doubleSpeed = false;
-            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
-            dirtParticle.Play();
-        }*/
+            if (isSliding)
+            {
+                // Terminar el deslizamiento
+                isSliding = false;
+                playerRb.height *= 2f;
+                playerRb.center = new Vector3(playerRb.center.x, playerRb.center.y * 2f, playerRb.center.z);
+
+                /*doubleSpeed = false;
+                playerAnim.SetFloat("Speed_Multiplier", 1.5f);
+                dirtParticle.Play();*/
+            }
+        }
         
     }
 
-    /*private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
-            dirtParticle.Play();
-            hasDoubleJump = false;
+            /*dirtParticle.Play();
+            hasDoubleJump = false;*/
         } 
-        else if(collision.gameObject.CompareTag("Obstacle"))
+        /*else if(collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
@@ -70,10 +103,10 @@ public class PlayerController : MonoBehaviour
             explosionParticle.Play();
             dirtParticle.Stop();
             playerAudio.PlayOneShot(crashSound, 1.0f);
-        }
+        }*/
     }
 
-    void OnTriggerEnter(Collider other)
+    /*void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("PowerUp") && !isPowerUpActive)
         {
