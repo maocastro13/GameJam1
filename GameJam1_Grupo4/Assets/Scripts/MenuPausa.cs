968ab6b2359f2d6712ede6da0d896195d2cef19b
Mauricio Andrés Castro Morales <maocastro13@gmail.com>
2023-04-21 16:17:10 -0500

    EscenaYoleFinish

---
+++ GameJam1_Grupo4/Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(juegoPausado)
+            {
+                Reanudar();
+            }else
+            {
+                Pausar();
+            }
+
+
+        }
+    }
+
+    public void Pausar()
+    {
+        juegoPausado = true;
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive (true);
+    }
+
+    public void Reanudar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void Cerrar()
+    {
+        Debug.Log("Cerrando Juego");
+        Application.Quit();
+    }
+
+
+
+}
